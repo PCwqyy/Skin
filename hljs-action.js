faa9ef74214e2f05a39470390287ed1fec20c7bb
PCwqyy <w123567910@outlook.com>
2026-06-07 16:55:26 +0800

    copy

---
+++ hljs-action.js
@@ -0,0 +1,20 @@
+(()=>{
+	/** @param {Element} ele */
+	function AddCopyButton(ele)
+	{
+		var btn=document.createElement("button");
+		btn.classList.add("hljs-copy-btn");
+		btn.textContent="Copy";
+		btn.addEventListener("click",()=>{
+			navigator.clipboard.writeText(ele.textContent);
+			btn.textContent="Copied!";
+			setTimeout(()=>btn.textContent="Copy",2000);
+		});
+		ele.before(btn);
+	}
+	window.hljs.addActions=()=>{
+		document.querySelectorAll("pre>code").forEach((ele)=>{
+			AddCopyButton(ele);
+		});
+	}
+})();
